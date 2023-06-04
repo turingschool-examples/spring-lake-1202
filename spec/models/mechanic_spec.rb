d99d726ea3cefb63d35be969cd61e8ae2931a62c
Lauren Golden <llgolden@gmail.com>
2023-06-04 09:57:08 -0600

    Set up relationships and migrations

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,1 +1,8 @@
-start time: 9:43, end time: 12:43
+require "rails_helper"
+
+RSpec.describe Mechanic, type: :model do
+  describe "relationships" do
+    it { should have_many :ride_mechanics }
+    it { should have_many(:rides).through(:ride_mechanics) }
+  end
+end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Ride, type: :model do
   describe "relationships" do
     it { should belong_to(:amusement_park) }
+    it { should have_many :ride_mechanics }
+    it { should have_many(:mechanics).through(:ride_mechanics) }
   end
 end
