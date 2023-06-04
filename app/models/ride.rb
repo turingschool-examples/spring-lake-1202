42dc774da84ccc4a3ac28a80e6880d3056117e99
Steven Stackhouse <stackmm@gmail.com>
2023-06-04 14:06:50 -0600

    created user story 1 test data

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,5 +3,5 @@ class Ride < ApplicationRecord
   has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
 
-  validates_presence_of :name, :thrill_rating, :open
+  validates_presence_of :name, :thrill_rating
 end
