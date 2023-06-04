8e90b89a5d7499980c770a6b4a6863e5fb5cfe39
Steven Stackhouse <stackmm@gmail.com>
2023-06-04 13:40:28 -0600

    add all associations to models and model tests

--- spec/models/mechanic_ride_spec.rb
+++ spec/models/mechanic_ride_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe MechanicRide, type: :model do
   describe "relationships" do
-    
+    it {should belong_to(:mechanic)}
+    it {should belong_to(:ride)}
   end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Mechanic, type: :model do
   describe "relationships" do
-
+    it { should have_many(:mechanic_rides)}
+    it { should have_many(:rides).through(:mechanic_rides)}
   end
 end
