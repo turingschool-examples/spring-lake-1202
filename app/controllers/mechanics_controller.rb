f687428550b9a8f6f14589d5d7805a88c40391bb
Nick Tassinari <ntass7800@gmail.com>
2023-06-04 10:25:29 -0600

    user story 2

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,11 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def create
+    @mechanic = Mechanic.find(params[:id])
+    @ride = Ride.find(params[:ride_id])
+    MechanicRide.create!(ride_id: @ride.id, mechanic_id: @mechanic.id)
+    redirect_to "/mechanics/#{@mechanic.id}"
+  end
 end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,24 +1,39 @@
 require "rails_helper"
 
 RSpec.describe "Mechanics Show Page" do 
+  before(:each) do 
+    @six_flags = AmusementPark.create!(name: "Six Flags", admission_cost: 75)
+    @mechanic_1 = Mechanic.create!(name: "Kara Smith", years_experience: 11)
+    @mechanic_2 = Mechanic.create!(name: "Karl Snith", years_experience: 11)
+    @ride_1 = @six_flags.rides.create!(name: "The Hurler", thrill_rating: 7, open: false)
+    @ride_2 = @six_flags.rides.create!(name: "The UnFurler", thrill_rating: 7, open: false)
+    @ride_3 = @six_flags.rides.create!(name: "The Spewer", thrill_rating: 7, open: false)
+    @mech_rides = MechanicRide.create!(ride_id: @ride_1.id, mechanic_id: @mechanic_1.id)
+    @mech_rides = MechanicRide.create!(ride_id: @ride_2.id, mechanic_id: @mechanic_1.id)
+    @mech_rides = MechanicRide.create!(ride_id: @ride_3.id, mechanic_id: @mechanic_2.id)
+  end
+  #user story 1
   it "lists mechanic info" do 
-    six_flags = AmusementPark.create!(name: "Six Flags", admission_cost: 75)
-    mechanic_1 = Mechanic.create!(name: "Kara Smith", years_experience: 11)
-    mechanic_2 = Mechanic.create!(name: "Karl Snith", years_experience: 11)
-    ride_1 = six_flags.rides.create!(name: "The Hurler", thrill_rating: 7, open: false)
-    ride_2 = six_flags.rides.create!(name: "The UnFurler", thrill_rating: 7, open: false)
-    ride_3 = six_flags.rides.create!(name: "The Spewer", thrill_rating: 7, open: false)
-    mech_rides = MechanicRide.create!(ride_id: ride_1.id, mechanic_id: mechanic_1.id)
-    mech_rides = MechanicRide.create!(ride_id: ride_2.id, mechanic_id: mechanic_1.id)
-    mech_rides = MechanicRide.create!(ride_id: ride_3.id, mechanic_id: mechanic_2.id)
 
-    visit "/mechanics/#{mechanic_1.id}"
+    visit "/mechanics/#{@mechanic_1.id}"
 
-    expect(page).to have_content(mechanic_1.name)
-    expect(page).to have_content(mechanic_1.years_experience)
-    expect(page).to have_content(ride_1.name)
-    expect(page).to have_content(ride_2.name)
-    expect(page).to_not have_content(ride_3.name)
+    expect(page).to have_content(@mechanic_1.name)
+    expect(page).to have_content(@mechanic_1.years_experience)
+    expect(page).to have_content(@ride_1.name)
+    expect(page).to have_content(@ride_2.name)
+    expect(page).to_not have_content(@ride_3.name)
+    expect(page).to_not have_content(@mechanic_2.name)
+  end
 
+  #user story 2
+  it "has a form to add ride to workload" do 
+
+    visit "/mechanics/#{@mechanic_1.id}"
+
+    expect(page).to have_content("Add Ride To Workload")
+    fill_in("Ride", with: "#{@ride_3.id}")
+    click_button "Submit"
+    expect(current_path).to eq("/mechanics/#{@mechanic_1.id}")
+    expect(page).to have_content(@ride_3.name)
   end
 end
