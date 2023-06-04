6cbd656285a5bb27fb87e774c2c81897660e5683
Javen Wilson <javenb022@gmail.com>
2023-06-04 12:26:50 -0400

    Refactored unique_mechanics method

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -2,6 +2,6 @@ class AmusementPark < ApplicationRecord
   has_many :rides
 
   def unique_mechanics
-    rides.joins(:mechanics).select('mechanics.name').distinct
+    rides.joins(:mechanics).distinct.pluck('mechanics.name')
   end
 end
