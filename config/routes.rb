0de68bf9c50b4589b9694c1eed4c37eb901fcee1
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-05 17:59:29 -0600

    add view feature test file and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/mechanic/:id", to: "mechanics#show"
+
+  get "/amusement_park/:id", to: "amusement_park#show"
 end
