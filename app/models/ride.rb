6af0ca919ee6596bb8de260ac35975be927e2a9b
Nick Sacco <nicksacco10@gmail.com>
2023-06-05 14:11:58 -0700

    test/feat: avg exp moved to ride model with tests

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,8 @@ class Ride < ApplicationRecord
   belongs_to :amusement_park
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
+
+  def avg_exp
+    self.mechanics.average(:years_experience).to_f
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,17 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Ride, type: :model do
+  before(:each) do
+    test_data
+  end
+
   describe "relationships" do
     it { should belong_to(:amusement_park) }
     it { should have_many(:ride_mechanics)}
     it { should have_many(:mechanics).through(:ride_mechanics)}
   end
 
-  describe "instance methods" do
-    describe "#avg_exp" do
-      it "can determine average experience of mechanics for a ride" do
-        
-      end
+  describe "#avg_exp" do
+    it "can determine average experience of mechanics for a ride" do
+      expect(@ride1.avg_exp).to eq(25.5)
     end
   end
 end
