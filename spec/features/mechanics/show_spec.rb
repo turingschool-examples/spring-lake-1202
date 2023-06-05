1890b3cc5fdb3ec5e0d8c51c903e54b18a375a5f
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-04 18:37:23 -0600

    Test: Add more test data to mechanics show

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -14,20 +14,39 @@ RSpec.describe "the mechanics show page" do
   let!(:ride_mechanic_2) { RideMechanic.create!(ride: ride_2, mechanic: mechanic_1) }
   let!(:ride_mechanic_3) { RideMechanic.create!(ride: ride_3, mechanic: mechanic_2) }
 
-  before(:each) do
-    visit mechanic_path(mechanic_1)
+  context "Mechanic 1" do
+    it "displays the mechanic's name, years of experience, and the names of all the rides they are working on" do
+      visit mechanic_path(mechanic_1)
+
+      expect(page).to have_content(mechanic_1.name)
+      expect(page).to have_content("Years of Experience: #{mechanic_1.years_experience}")
+
+      expect(page).to have_content("Rides they are working on:")
+      within("#rides") do
+        expect(page).to have_content(ride_1.name)
+        expect(page).to have_content(ride_2.name)
+      end
+
+      expect(page).not_to have_content(mechanic_2.name)
+      expect(page).not_to have_content(ride_3.name)
+    end
   end
 
-  it "displays the mechanic's name, years of experience, and the names of all the rides they are working on" do
-    expect(page).to have_content(mechanic_1.name)
-    expect(page).to have_content("Years of Experience: #{mechanic_1.years_experience}")
-    expect(page).to have_content("Rides working on")
-    within("#rides-working-on") do
-      expect(page).to have_content(ride_1.name)
-      expect(page).to have_content(ride_2.name)
-    end
+  context "Mechanic 2" do
+    it "displays the mechanic's name, years of experience, and the names of all the rides they are working on" do
+      visit mechanic_path(mechanic_2)
 
-    expect(page).not_to have_content(mechanic_2.name)
-    expect(page).not_to have_content(ride_3.name)
+      expect(page).to have_content(mechanic_2.name)
+      expect(page).to have_content("Years of Experience: #{mechanic_2.years_experience}")
+
+      expect(page).to have_content("Rides they are working on:")
+      within("#rides") do
+        expect(page).to have_content(ride_3.name)
+      end
+
+      expect(page).not_to have_content(mechanic_1.name)
+      expect(page).not_to have_content(ride_1.name)
+      expect(page).not_to have_content(ride_2.name)
+    end
   end
 end
