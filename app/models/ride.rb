b0b57decd870d19ab166fa926e235fb9c9ec1785
Andy Weissman <andyweissman6@gmail.com>
2023-06-05 13:27:35 -0600

    relationships complete

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,5 @@
 class Ride < ApplicationRecord
   belongs_to :amusement_park
+  has_many :ride_mechanics
+  has_many :mechanics, through: :ride_mechanics
 end
---
+++ db/migrate/20230605191836_ride_mechanics.rb
@@ -0,0 +1,4 @@
+class RideMechanics < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
