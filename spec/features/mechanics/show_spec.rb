0b6a4460c8a9800fa65a42013f3fea2ea10e9c20
Nick Sacco <nicksacco10@gmail.com>
2023-06-05 12:56:51 -0700

    test: add ride to mechanic tests written

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -5,13 +5,25 @@ RSpec.describe "Mechanic Show Page", type: :feature do
     test_data
   end
 
-  it "Can show the selected mechanic attributes" do
+  it "can show the selected mechanic attributes" do
     visit "/mechanics/#{@mech1.id}"
-
+    
     expect(page).to have_content("Name: #{@mech1.name}")
     expect(page).to have_content("Years of Experience: #{@mech1.years_experience}")
     expect(page).to have_content(@ride1.name)
     expect(page).to have_content(@ride2.name)
     expect(page).to_not have_content(@mech2.name)
+  end
+  
+  it "can add a ride to a mechanic by ride id" do
+    visit "/mechanics/#{@mech4.id}"
+
+    expect(page).to have_content("Add Ride by ID")
+
+    fill_in(:ride_id, with: "#{@ride3.id}")
+    click_button("Add")
+
+    expect(current_path).to eq("/mechanics/#{@mech4.id}")
+    expect(page).to have_content(@ride3.name)
   end
 end
