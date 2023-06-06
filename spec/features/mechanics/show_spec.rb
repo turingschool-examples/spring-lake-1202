fb000cdc6439d52f60960738dfbd4d69b70efac7
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-06-06 15:57:30 -0600

    Test: mechanic show page.

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "the mechanics show page" do 
+  before(:each) do
+    @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+
+    @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: false)
+    @pukie = @six_flags.rides.create!(name: 'The Pukie', thrill_rating: 9, open: true)
+
+    @bob = Mechanic.create!(name: "Bob", years_experience: 10)
+
+    @bob_hurler = MechanicsRide.create!(ride: @hurler, mechanic: @bob)
+    @bob_pukie = MechanicsRide.create!(ride: @pukie, mechanic: @bob)
+  end
+
+  describe "as a user" do 
+    it "renders the mechanic's name, years of experience, and the names of rides they are working on" do 
+      visit "/mechanics/#{@bob.id}"
+      expect(page).to have_content("Mechanic: #{@bob.name}")
+      expect(page).to have_content("Years of experience: #{@bob.years_experience}")
+      expect(page).to have_content("Working on: #{@hurler.name}")
+      expect(page).to have_content("Working on: #{@pukie.name}")
+    end
+  end
+end
