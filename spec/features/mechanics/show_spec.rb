42629de7d20b3c6d27bacab1b042eaed9fc5ed3f
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-06-06 16:33:34 -0600

    Test: rendering add a ride form.

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -13,15 +13,29 @@ RSpec.describe "the mechanics show page" do
     @bob_pukie = MechanicsRide.create!(ride: @pukie, mechanic: @bob)
   end
 
-  describe "as a user" do 
-    it "renders the mechanic's name, years of experience, and the names of rides they are working on" do 
-      visit "/mechanics/#{@bob.id}"
+  describe "as a user" do
+    context "mechanic header" do 
+      it "renders the mechanic's name, years of experience, and the names of rides they are working on" do 
+        visit "/mechanics/#{@bob.id}"
 
-      within(".mechanic_header") do 
-        expect(page).to have_content("Mechanic: #{@bob.name}")
-        expect(page).to have_content("Years of experience: #{@bob.years_experience}")
-        expect(page).to have_content("#{@hurler.name}")
-        expect(page).to have_content("#{@pukie.name}")
+        within(".mechanic_header") do 
+          expect(page).to have_content("Mechanic: #{@bob.name}")
+          expect(page).to have_content("Years of experience: #{@bob.years_experience}")
+          expect(page).to have_content("#{@hurler.name}")
+          expect(page).to have_content("#{@pukie.name}")
+        end
+      end
+    end
+
+    context "Add a ride form" do 
+      it "renders a form to add an existing ride to a mechanic's workload" do 
+        visit "/mechanics/#{@bob.id}"
+
+        within(".add_ride_form") do 
+          expect(page).to have_content("Add a Ride")
+          expect(page).to have_content("ID")
+          expect(page).to have_button("Submit")
+        end
       end
     end
   end
