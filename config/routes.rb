9e125ffaaca9ba6a971423d79b940f8b47db2356
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-06 14:52:10 -0600

    add rides_mechanics/new route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "mechanics/:id", to: "mechanics#show"
+
+  post "ride_mechanics/new", to: "ride_mechanics#create"
 end
