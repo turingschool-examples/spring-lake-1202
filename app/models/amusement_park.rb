2d2c67502f044aea80c25abb1e1acbbd6a49c574
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-05 20:06:20 -0600

    add method for mech working on rides

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,7 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  def mechanics_working_on_rides
+    Mechanic.joins(:mechanic_rides).where(mechanic_rides: { ride_id: self.rides })
+  end
 end
