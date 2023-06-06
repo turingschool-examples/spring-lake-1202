5a0f0646bc94a996cf6186add9fa38698fcf24c3
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-06 16:31:21 -0600

    refactor route for helpers

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/mechanics/:id", to: "mechanics#show"
+  # get "/mechanics/:id", to: "mechanics#show"
+  
+  resources :mechanics, only: [:show]
 end
