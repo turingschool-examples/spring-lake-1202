480abe9e3bc20059028bae61fc0c5ebb80ef5427
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-05 19:10:44 -0600

    add mechanics query

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,4 +1,10 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
   has_many :ride_mechanics, through: :rides
+
+  def mechanics_working
+    rides.joins(:ride_mechanics, :mechanics)
+    .select('mechanics.name')
+    .distinct.pluck('mechanics.name')
+  end
 end
