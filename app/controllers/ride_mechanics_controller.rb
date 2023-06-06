ab27149d6f31fd4129bb49f7057ac09dc56e16eb
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-06 15:31:20 -0600

    create amusement_parks controller

--- app/controllers/ride_mechanics_controller.rb
+++ app/controllers/ride_mechanics_controller.rb
@@ -4,9 +4,4 @@ class RideMechanicsController < ApplicationController
     RideMechanic.create!(ride_id: params[:ride_id], mechanic_id: params[:mechanic_id])
     redirect_to "/mechanics/#{mechanic.id}"
   end
-
-  # private
-  # def ride_mechanics_params
-  #   params.permit(:ride_id, :mechanic_id)
-  # end
 end
