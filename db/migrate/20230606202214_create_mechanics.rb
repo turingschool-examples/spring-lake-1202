c2176f3af1bfec637b1619d6ec4dc4dbe93df12a
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-06 14:23:13 -0600

    create mechanics table

---
+++ db/migrate/20230606202214_create_mechanics.rb
@@ -0,0 +1,11 @@
+class CreateMechanics < ActiveRecord::Migration[7.0]
+  def change
+    create_table :mechanics do |t|
+      t.string :name
+      t.string :years_experience
+      t.references :ride, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
