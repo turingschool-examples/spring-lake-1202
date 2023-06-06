1eeeb5988627875a74205c19f8b1d4ec6c6f672b
Julian Beldotti <julian.beldotti@gmail.com>
2023-06-05 23:15:32 -0500

    EX1 End: 23:15

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -5,4 +5,11 @@ class AmusementPark < ApplicationRecord
   def distinct_mechanics
     mechanics.distinct
   end
+
+  def avg_mech_rides
+    Ride.joins(:mechanics)
+    .select("rides.*, avg(mechanics.years_experience) AS avg_years")
+    .group(:id)
+    .order("avg_years desc")
+  end
 end
--- spec/features/amusement_parks.rb/show_spec.rb
+++ spec/features/amusement_parks.rb/show_spec.rb
@@ -13,11 +13,11 @@ RSpec.describe 'amusement parks show page' do
   let!(:holy) { cedar.rides.create!(name: "Holy Roller", thrill_rating: 10, open: false) }
   let!(:me) { elitch.rides.create!(name: "Mind Eraser", thrill_rating: 9, open: true) }
 
-  let!(:ride_mech_1) { RideMechanic.create!(mechanic_id: mech_1.id, ride_id: log.id) }
-  let!(:ride_mech_2) { RideMechanic.create!(mechanic_id: mech_1.id, ride_id: mant.id) }
-  let!(:ride_mech_3) { RideMechanic.create!(mechanic_id: mech_2.id, ride_id: holy.id) }
-  let!(:ride_mech_4) { RideMechanic.create!(mechanic_id: mech_2.id, ride_id: mant.id) }
-  let!(:ride_mech_5) { RideMechanic.create!(mechanic_id: mech_3.id, ride_id: me.id) }
+  let!(:ride_mech_1) { RideMechanic.create!(mechanic_id: mech_1.id, ride_id: log.id) } #18
+  let!(:ride_mech_2) { RideMechanic.create!(mechanic_id: mech_1.id, ride_id: mant.id) } #18
+  let!(:ride_mech_3) { RideMechanic.create!(mechanic_id: mech_2.id, ride_id: holy.id) } #26
+  let!(:ride_mech_4) { RideMechanic.create!(mechanic_id: mech_2.id, ride_id: mant.id) } #22 avg
+  let!(:ride_mech_5) { RideMechanic.create!(mechanic_id: mech_3.id, ride_id: me.id) } #12
 
   describe 'displays the amusement parks attributes and mechanics working on rides there' do
     it 'should display the amusement parks name and admission fee' do
@@ -41,4 +41,22 @@ RSpec.describe 'amusement parks show page' do
       expect(page).to have_content(mech_3.name)
     end
   end
+
+  describe 'displays an ordered list of rides with mechanics and their average years worked' do
+    it 'should display an ordered list of rides by mechanics average expereince' do
+      visit amusement_park_path(cedar)
+      save_and_open_page
+      expect(holy.name).to appear_before(mant.name)
+      expect(mant.name).to appear_before(log.name)
+      expect(page).to have_content(18)
+      expect(page).to have_content(22)
+      expect(page).to have_content(26)
+    end
+  end
 end
+# As a visitor,
+# When I visit an amusement park's show page,
+# Then I see a list of all of the park's rides,
+# And next to the ride name I see the average experience of the mechanics working on the ride,
+# And I see the list of rides is ordered by the average experience of mechanics working on the ride.
+# (Note: you should only perform 1 database query to retrieve the list of rides with average mechanic experience)
