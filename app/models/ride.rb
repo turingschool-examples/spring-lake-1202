6796dcc206fc61e695ea4872d3d4b0577b049721
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-06 16:38:57 -0600

    Feat: #mechanic_avg_experience

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,8 @@ class Ride < ApplicationRecord
   belongs_to :amusement_park
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
+
+  def mechanic_avg_experience
+    mechanics.average(:years_experience).round(2)
+  end
 end
