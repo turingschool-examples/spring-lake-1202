fc05c52e9a74118d94d6ad2e69991d2f2062be32
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-05 18:52:07 -0600

    add update to mech controller

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -3,4 +3,14 @@ class MechanicsController < ApplicationController
     @mechanic = Mechanic.find(params[:id])
   end
 
+  def update
+    @mechanic = Mechanic.find(params[:id])
+    ride = Ride.find(params[:mechanic][:ride_id])
+
+    mechanic_ride = MechanicRide.new(mechanic: @mechanic, ride: ride)
+
+    if mechanic_ride.save
+      redirect_to mechanic_path(@mechanic), notice: "Ride added to mechanic's workload."
+    end
+  end
 end
