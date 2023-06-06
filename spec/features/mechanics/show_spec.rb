383226967b9f2b372c27857943efd407e01c4016
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-06 14:26:41 -0600

    finish user story 1

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Mechanic Show Page' do
   before(:each) do
-    @jelly = Mechanic.create!(name: "Jelly Parlor", years_experience: 12)
-    @turbinator = Ride.create!(name: 'The Turbinator', thrill_rating: 1, open: true)
-    @juicer = Ride.create!(name: 'The Juicer', thrill_rating: 9, open: false)
+    @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+    @jelly = @six_flags.mechanics.create!(name: "Jelly Parlor", years_experience: 12)
+    @turbinator = @six_flags.rides.create!(name: 'The Turbinator', thrill_rating: 1, open: true)
+    @juicer = @six_flags.rides.create!(name: 'The Juicer', thrill_rating: 9, open: false)
 
     RideMechanic.create!(ride_id: @juicer.id, mechanic_id: @jelly.id)
   end
@@ -14,6 +15,9 @@ RSpec.describe 'Mechanic Show Page' do
 
     expect(page).to have_content(@jelly.name)
     expect(page).to have_content("Years of Experience: #{@jelly.years_experience}")
-    expect(page).to have_content("Rides Responsible: #{@juicer.name}")
+    within("#rides") do
+      expect(page).to have_content("Rides Responsible:")
+      expect(page).to have_content("#{@juicer.name}")
+    end
   end
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe AmusementPark, type: :model do
   describe "relationships" do
     it { should have_many(:rides) }
+    it { should have_many(:mechanics) }
   end
 end
