f0b4bd997e5b94452d8df7fb5ccf3ab73d6e3fe8
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-06 18:38:18 -0600

    Refactor: Clean up per rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 AmusementPark.destroy_all
 Ride.destroy_all
 
-@six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
-@universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+@six_flags = AmusementPark.create!(name: "Six Flags", admission_cost: 75)
+@universal = AmusementPark.create!(name: "Universal Studios", admission_cost: 80)
 
-@hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
-@scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
-@ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+@hurler = @six_flags.rides.create!(name: "The Hurler", thrill_rating: 7, open: true)
+@scrambler = @six_flags.rides.create!(name: "The Scrambler", thrill_rating: 4, open: true)
+@ferris = @six_flags.rides.create!(name: "Ferris Wheel", thrill_rating: 7, open: false)
 
-@jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+@jaws = @universal.rides.create!(name: "Jaws", thrill_rating: 5, open: true)
